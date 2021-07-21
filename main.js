677a7b17a137064a79acc864cbf159dc79b34d60
ksn-design <65951286+ksn-design@users.noreply.github.com>
2021-07-22 01:11:31 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,13 @@
-// メニュー
-class MobileMenu{
-    constructor (){
-        this.DOM = {};
-        this.DOM.btn = document.querySelector('.mobile-menu__btn');
-        this.DOM.cover = document.querySelector('.mobile-menu__cover');
-        this.DOM.btn.container = document.querySelector('#global-container');
-        this.eventType= this._getEventType();
-        this._addEvent();
-    }
+//メニュー
 
-    _getEventType(){
-        return window.ontouchstart ? 'touchstart' : 'click';
-    }
+btn = document.querySelector(".mobile-menu__btn");
+btn.addEventListener("click", ()=>{
+    document.querySelector(".mobile-menu__btn").classList.toggle("active");
+    document.querySelector(".mobile-menu").classList.toggle("menu-open");
+    
+});
 
-    _toggle()  {
-        this.DOM.btn.container.classList.toggle('menu-open');
-    }  
-    _addEvent(){
-        this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
-        this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
-    }
-}
 
-new MobileMenu ();
 
 // ここからスライダー
 document.addEventListener('DOMContentLoaded', function () {
@@ -68,3 +53,4 @@ class HeroSlider {
         this.swiper.autoplay.stop();
     }
 }
+
